261edb87716ef1d8b70b0212f0ded27355e074ee
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-07-08 14:56:14 -0700

    updating heroku routes

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -7,11 +7,13 @@ const Print = require('../models/print.js');
 // routes
 //////////////////////////////////////////////////////////////////////////////////
 
+// Cart.create([{idArray: []} ], (err, data)=>{
+//     res.redirect('/prints');
+// })
+// create seed?
+
 // display cart
 router.get('/', (req,res) => {
-    Cart.create([{idArray: []} ], (err, data)=>{
-        res.redirect('/prints');
-    })
     Cart.findOne({}).populate('idArray').then( (found) => {
         res.render('checkout.ejs', {
             cart : found.idArray
@@ -33,26 +35,5 @@ router.get('/add/:id', (req,res) => {
         res.redirect('/prints/'+req.params.id);
     });
 });
-
-// // update from edit page
-// router.put('/:id', (req,res) => {
-//     Cart.findOneAndUpdate(req.params.id,req.body,{new:true},(err,updated) => {
-//         res.redirect('/prints/'+req.params.id);
-//     });
-// });
-
-// // delete
-// router.delete('/:id', (req, res)=>{
-//     Cart.findOneAndUpdate({},{$pull: { idArray : {$elemMatch : {_id : req.params.id}}}},{new: true},(err, data)=>{
-//         res.redirect('/cart/');
-//     });
-// });
-
-// // new product details
-// router.get('/new', (req,res) => {
-//     res.send("new print admin");
-//     // res.render("new.ejs");
-// });
-
 
 module.exports = router;
